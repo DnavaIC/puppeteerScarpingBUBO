40f5a1baa9926172d566338fba0e9153a4c85f6c
Diego Nava <dnava@icsecurity.com>
2025-05-21 15:08:17 -0600

    Add: navigation bot completed

--- officers.js
+++ officers.js
@@ -1,15 +1,66 @@
 import puppeteer from "puppeteer";
+import dotenv from "dotenv";
+import delay from "./utils/timeout.js";
 
 (async () => {
+
+    // Locators
+    const buboAccess = 'button[type="button"]'
+    const corporateLogin = '::-p-aria([name="ICS-Corporate-Login"])'
+    const emailInput = 'input[type="email"]'
+    const passwdInput = 'input[name="passwd"]'
+    const submitButton = '#idSIButton9'
+    const loginMS = '.button_primary'
+    const officerSwitch = 'button[role="switch"]'
+    
+    dotenv.config();
+    // Browser settings
     const browser = await puppeteer.launch({
         headless: false,
         args: ['--start-maximized'],
         defaultViewport: null
     });
+
+    // Start script 
+    // Launching browser
     const page = await browser.newPage();
 
+    // Screen 1 - Bubo access
+    // Opening bubo gantt acces page
     console.log("opening page");
-    await page.goto("https://gantt-qa.bubo.io/#/login");
+    await page.goto(process.env.BUBO_URL);
 
-    await page.click('button[type="button"]');
+    // Screen 1
+    // Clicking on the first login button
+    console.log("Step 1: Click Bubo login button");
+    
+    await page.click(buboAccess);
+
+    //Screen 2 - corporate login page
+    // Clicking corporate login button
+    console.log("Step 2: Click corporate login button");
+    await page.locator(corporateLogin).click();
+
+    //Screen 3 - MS login 
+    //Filling login form
+    console.log("Step 3: input email and password");
+    await page.waitForSelector(emailInput,  {timeout: 5000});
+    await page.type(emailInput, process.env.EMAIL);
+
+    await page.waitForSelector(submitButton,  {timeout: 5000});
+    await page.click(submitButton);
+
+    console.log("wait for page render");
+    await delay(4000);
+    
+    await page.waitForSelector(passwdInput, {timeout: 5000});
+    await page.type(passwdInput, process.env.PASSWRD);
+
+    await page.waitForSelector(loginMS, {timeout: 5000});
+    await page.click(loginMS);
+    
+    //Welcome to bubo page
+    console.log("Step 4: toggle officer switch");
+    await page.waitForSelector(officerSwitch, {timeout: 5000});
+    await page.click(officerSwitch);
 })();
